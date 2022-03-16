5fde2e73dcf882da4a5e16f98353dad1f7b00aab
alutton <andrew.lutton@gmail.com>
2022-03-16 12:41:19 -0400

    Add basic display handling for numbers

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let displayValue = "";
+let historyValue = "";
+
+historyDisplay = document.querySelector(".history-display");
+mainDisplay = document.querySelector(".main-display");
+
+buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => button.addEventListener("click", displayHandler));
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -37,4 +46,22 @@ function operate(operator, num1, num2) {
             break;
     }
     return result;
+}
+
+function displayHandler() {
+    switch (true) {
+        case this.classList.contains("num-btn"):
+            displayValue += this.value;
+            mainDisplay.textContent = displayValue;
+            break;
+        case this.classList.contains("operator-btn"):
+            break;
+        case this.classList.contains("display-btn"):
+            break;
+        case this.classList.contains("plus-minus-btn"):
+            break;
+        default:
+            console.log("Not num-btn");
+            break;
+    }
 }
