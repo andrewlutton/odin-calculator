52cf224e1842e4fe269ea5a15e7428dcc943a887
alutton <andrew.lutton@gmail.com>
2022-03-16 11:32:24 -0400

    Add operations functions

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Hello World");
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function backspace(){}
+
+function clear(){}
+
+function operate(operator, num1, num2) {
+    let result = undefined;
+    switch (operator) {
+        case "add":
+            result = add(num1, num2);
+            break;
+        case "subtract":
+            result = add(num1, num2);
+            break;
+        case "multiply":
+            result = add(num1, num2);
+            break;
+        case "divide":
+            result = add(num1, num2);
+            break;
+        default:
+            break;
+    }
+    return result;
+}
